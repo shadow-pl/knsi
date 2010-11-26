aef69b4eef6999f687a69472d5203aa1109f7f57
Maciej Smolinski <msblaze89@gmail.com>
2010-11-26 13:51:49 +0100

    Devise setup complete (rails generate devise:install)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Knsi::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Devise mail server setup
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
