7535c1b604157aea0ecc213627de0292ab705d05
Maciej Smolinski <msblaze89@gmail.com>
2010-11-26 13:29:06 +0100

    Initial Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knsi::Application.config.secret_token = '62903043839ac94654d754e641be88f1b3b8a02d8b866d59297f18cfca4f1a2d18ef0fbd3ff845d5a4b234923e74805f4f57feb27a108edf75f080a29a9a7169'
